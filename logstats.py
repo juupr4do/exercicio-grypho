9840e026a60bc6a6ac44c7ad5ebf89d7e3e165f3
julia <ju.pprado@alumni.usp.br>
2026-08-03 22:23:28 -0300

    ajustes e finalização do logstats

--- logstats.py
+++ logstats.py
@@ -2,6 +2,7 @@ import argparse
 import re
 import sys
 from collections import Counter
+import statistics
 
 LOG_PATTERN = re.compile(
     r'(?P<ip>\S+) \S+ \S+ \[(?P<timestamp>[^\]]+)\] ' +
@@ -32,6 +33,7 @@ def main():
     quatroXX = 0
     cincoXX = 0
     endpoints = Counter()
+    tempoResposta = []
 
     arquivo = abrir_log(args.logfile)
     with arquivo:
@@ -44,10 +46,14 @@ def main():
                 processadas += 1
                 status = match.group("status")
                 request = match.group("request")
+                tempo = match.group("response_time")
 
                 metodo, endpoint, protocolo = request.split()
                 endpoints[endpoint] += 1
                 top5 = endpoints.most_common(5)
+
+                if tempo:
+                    tempoResposta.append(float(tempo))
 
                 if status[0] == '2':
                     doisXX += 1
@@ -60,14 +66,25 @@ def main():
 
             continue
 
+    tempoResposta.sort(reverse=True)
+    top5Time = tempoResposta[:5]
+
+    if tempoResposta:
+        p95 = statistics.quantiles(
+            tempoResposta,
+            n=100
+        )[94]
+    else:
+        p95 = 0
+
     print(f"Processadas: {processadas}")
     print(f"Descartadas: {descartadas}")
     print(f"Top 5 endpoints acessados: {top5}")
-    print(f"5xx: {cincoXX}")
+    print(f"Top 5 tempo de acesso: {top5Time}")
+    print(f"p95: {p95:.3f} segundos")
     print(f"2xx: {doisXX}")
     print(f"3xx: {tresXX}")
     print(f"4xx: {quatroXX}")
-    print(f"5xx: {cincoXX}")
     print(f"5xx: {cincoXX}")
 
 main()
